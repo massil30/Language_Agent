c27c8ce948da6adf6d8a835c1f642978ade1121a
massil30 <massilattaf04@gmail.com>
2025-10-31 07:04:44 +0100

    add user Input to Prompt

--- formule.py
+++ formule.py
@@ -1,19 +1,26 @@
-from langchain_community.llms import OpenAI
 import os
 from print import info, error, success
 from prompts import Prompts
 from dotenv import load_dotenv
 
 #from openai import OpenAI
-
+from langchain_community.llms import OpenAI
 import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+#from langchain.schema import SystemMessage, HumanMessage
+
+#Load Api Key
 load_dotenv()
 # Configure Gemini with API key from .env file
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 info("-----------  Loading  ------------")
 model = genai.GenerativeModel("models/gemini-2.5-flash")
-
-info('-----------      --------------')
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that answers in French with short answers no extra explanation. "),
+    ("human", "{question}")
+])
+info('--------------------------------------')
 text = input("Add you're sentence here: ")
 info(text)
 prompt =Prompts.manieres
@@ -29,4 +36,5 @@ except Exception as e:
     error(f"count_tokens not supported: {e}")
 
 response = model.generate_content(full_prompt)
+#print(f"\n🧠 Response:")
 success(f"Output:\n{response.text}")
